71cdbfc4362cd1a92235305481cf066df1e9eef1
Eugene Chichilenko <johnnysuperb@mail.ru>
2022-02-06 01:41:22 +0300

    Solve VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      CalcX: 1,
+      CalcY: 1,
+      CalcZ: 2,
+      Operand: 'sum',
+    };
+  },
+
+  methods: {
+    CalculateGeneral(Operand) {
+      if (Operand == undefined) Operand = this.Operand;
+      switch (Operand) {
+        case 'sum':
+          this.CalcZ = this.CalcX + this.CalcY;
+          break;
+        case 'subtract':
+          this.CalcZ = this.CalcX - this.CalcY;
+          break;
+        case 'multiply':
+          this.CalcZ = this.CalcX * this.CalcY;
+          break;
+        case 'divide':
+          this.CalcZ = this.CalcY == 0 ? 0 : this.CalcX / this.CalcY;
+          break;
+      }
+    },
+  },
+}).mount('#app');
